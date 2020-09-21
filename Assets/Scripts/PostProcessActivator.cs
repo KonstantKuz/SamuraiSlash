97f2ae5195ec5d500d0e7eac347419372bfca840
Konstantin Kuz <stalkercatkuz@gmail.com>
2020-09-21 14:17:25 +0300

    fixed post process settings

--- Assets/Scripts/PostProcessActivator.cs
+++ Assets/Scripts/PostProcessActivator.cs
@@ -2,18 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class PostProcessActivator : MonoBehaviour
 {
+    [SerializeField] private bool useVignette;
     [SerializeField] private Volume postProcess;
 
+    private Vignette vignette;
+    
     private void OnEnable()
     {
         Observer.Instance.OnEnemyDied += ActivatePostProcess;
-        //Observer.Instance.OnCheckPointPassed += DisablePostProcess;
         Observer.Instance.OnWinLevel += DisablePostProcess;
     }
 
+    private void OnValidate()
+    {
+        postProcess.profile.TryGet(out vignette);
+        if (vignette != null)
+        {
+            vignette.active = useVignette;
+        }
+    }
     private void ActivatePostProcess()
     {
         postProcess.enabled = true;
